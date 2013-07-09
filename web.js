eac61249e16c883bb09d65a44db41dfa84d1dd65
Cameron Walker <cameron_walker01@yahoo.com.au>
2013-07-09 12:09:50 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
+var buf = require('buffer');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var str=buf.toString(fs.readFileSync("index.html"));
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
